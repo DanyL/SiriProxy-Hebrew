b46c9e72b7e1d0914fa0cb51d793c1c5e98f1f99
danyl <dany931@gmail.com>
2011-12-22 11:44:10 +0200

    More functions added comments next to each command.

--- lib/siriproxy-hebrew.rb
+++ lib/siriproxy-hebrew.rb
@@ -21,7 +21,7 @@ class SiriProxy::Plugin::Hebrew < SiriProxy::Plugin
     request_completed
   end
 
-  listen_for /shitty/i do
+  listen_for /shitty/i do #שירי - Shiri
     say "כן אדוני, כאן לשירותך!", spoken: "ken adoni, kan le shirutcha"
 
     request_completed
@@ -33,37 +33,55 @@ class SiriProxy::Plugin::Hebrew < SiriProxy::Plugin
     request_completed
   end
 
-  listen_for /shalom/i do
+  listen_for /shalom/i do #שלום - Shalom
     say "שלום DanyL", spoken: "shalom DanyL"
 
     request_completed
   end
 
-  listen_for /money anime/i do
+  listen_for /money anime/i do #מה העניינים - How you doing
     say "הכל טוב", spoken: "Hakol tov"
 
     request_completed
   end
 
-  listen_for /ma/i do
+  listen_for /map/i do #מה - What
     say "מה ששמעת!", spoken: "ma she'sha'mata!"
 
     request_completed
   end
 
-  listen_for /sasha/i do
+  listen_for /Llama/i do #למה - Why
+    say "ככה!", spoken: "kaha!"
+
+    request_completed
+  end
+
+  listen_for /sasha/i do #שרה שרה שיר שמח - Hebrew Tongue Twisters (such as "how much wood would a woodchuck... in english, but much shorter)
     say "שיר שמח שרה שרה", spoken: "Shir sameah shara sara"
 
     request_completed
   end
 
-  listen_for /the shitty/i do
+  listen_for /easily/i do #עברית - Hebrew
+    say "כן אני מדברת עברית", spoken: "ken, ani medaberet ivrit"
+
+    request_completed
+  end
+
+  listen_for /and/i do #אן דן דינו - Eeny meeny miny moe
+    say "אן דן דינו, סוף על הקטינו\nסוף על הקטי קטו\nאליק בליק בום.", spoken: "and, den, dino, sof, al, hakatino, sof, al, ha kati kato, elik, belik, bom"
+
+    request_completed
+  end
+
+  listen_for /shit/i do #שיר - Song
     say "כל עוד בלבב פנימה\nנפש יהודי הומיה\nולפאתי מזרח קדימה\nעין לציון צופיה\nעוד לא אבדה תקותנו\nהתקוה בת שנות אלפים\nלהיות עם חופשי בארצנו\nארץ ציון וירושלים", spoken: "Kol ode balevav \nP'nimah, - \n\nNefesh Yehudi homia, \n\nUlfa'atey mizrach kadimah, \nAyin l'tzion tzofia. \n\nOde lo avdah tikvatenu \nHatikvah bat shnot alpayim: \n\nL'hiyot am chofshi b'eartz'enu, - \nEretz Tzeion ve Yerushalayim."
 
     request_completed
   end
 
-  listen_for /bye/i do
+  listen_for /bye/i do #ביי - bye
     say "להתראות iPhoneIL", spoken: "lehitraot iPhone I L"
 
     request_completed
